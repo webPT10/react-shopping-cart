bc00da57d07c662c6aa94c6fb837073526bf4143
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-18 18:03:52 -0800

    step 4 -- imported useContext + ProductContext, passed in context object, removed all instances of prop.

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,5 +1,4 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
-const ProductContext = createContext();
+export const ProductContext = createContext();
 
-export default ProductContext;
